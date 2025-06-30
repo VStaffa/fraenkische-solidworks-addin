e3d1ef5e591098fe97173ed44a3d2c8c94918a76
staffav <staffav@HW3622>
2025-06-30 11:41:10 +0200

    Added taskpane manual buttons

--- fraenkischeAddin/UI/TaskpaneHostUI.cs
+++ fraenkischeAddin/UI/TaskpaneHostUI.cs
@@ -9,6 +9,7 @@ namespace Fraenkische.SWAddin.UI
     [ProgId(SWAddinClass.SWTASKPANE_PROGID)]
     public partial class TaskpaneHostUI : UserControl
     {
+        private Button button2;
         private Button button1;
 
         public TaskpaneHostUI()
@@ -19,11 +20,12 @@ namespace Fraenkische.SWAddin.UI
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 26);
+            this.button1.Location = new System.Drawing.Point(175, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(149, 33);
             this.button1.TabIndex = 0;
@@ -31,8 +33,19 @@ namespace Fraenkische.SWAddin.UI
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "BOM Export MANUAL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // TaskpaneHostUI
             // 
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "TaskpaneHostUI";
             this.Size = new System.Drawing.Size(343, 517);
@@ -46,6 +59,14 @@ namespace Fraenkische.SWAddin.UI
             var manualPath = Path.Combine(basePath, "AUTOKoch - USER MANUAL.pdf");
             
             Process.Start(new ProcessStartInfo(manualPath) {UseShellExecute = true });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var basePath = Path.Combine(Path.GetDirectoryName(typeof(SWAddinClass).Assembly.Location), @"Resources\Manuals");
+            var manualPath = Path.Combine(basePath, "BOM Export - MANUAL.pdf");
+
+            Process.Start(new ProcessStartInfo(manualPath) { UseShellExecute = true });
         }
     }
 }
